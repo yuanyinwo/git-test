20b256621542745ecfc9a3c80b05963c28967253
liyang <3145516878@qq.com>
2022-05-16 18:52:24 +0800

    first commit

---
+++ src/main/java/com/softeem/GitTest.java
@@ -0,0 +1,11 @@
+package com.softeem;
+
+/**
+ * @author lystart
+ * @create 2022-05-16 18:49
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
